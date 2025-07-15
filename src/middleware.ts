22447fb35969a573b543e7d4b67255259b1e9a30
NHeroOP <neeravtheheroo@gmail.com>
2025-07-15 19:23:24 +0530

    fixing the getToken problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,11 @@ import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt";
  
 export async function middleware(req: NextRequest ) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-  console.log("testing is token present in deployement", token)
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === 'production' ? true : false,
+  });
 
   const isLoggedIn = !!token
   const url = req.nextUrl
