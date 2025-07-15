c53ac8e28959b28c666369f499ee995f53635e92
NHeroOP <neeravtheheroo@gmail.com>
2025-07-15 19:19:25 +0530

    testing is token available in middleware when deployed

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { getToken } from "next-auth/jwt";
  
 export async function middleware(req: NextRequest ) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  console.log("testing is token present in deployement", token)
 
   const isLoggedIn = !!token
   const url = req.nextUrl
